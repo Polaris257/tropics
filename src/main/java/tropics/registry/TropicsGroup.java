382ad275dfa489555d8f56036a64b1e0bb17cae5
Expora09 <lennyj2468+github@gmail.com>
2021-03-27 21:07:32 +0000

    hopefully fixed itemgroup

--- src/main/java/tropics/registry/TropicsGroup.java
+++ src/main/java/tropics/registry/TropicsGroup.java
@@ -1,4 +1,19 @@
 package tropics.registry;
 
+import net.fabricmc.fabric.api.client.itemgroup.FabricItemGroupBuilder;
+import net.minecraft.item.ItemGroup;
+import net.minecraft.item.ItemStack;
+import net.minecraft.util.Identifier;
+
+import static tropics.registry.TropicsBlocks.BALSA_PLANKS;
+import static tropics.registry.TropicsItems.MACHETE;
+
 public class TropicsGroup {
+    public static final ItemGroup TROPICS_GROUP;
+    static {
+        TROPICS_GROUP = FabricItemGroupBuilder.create(new Identifier("tropics", "main")).icon(() -> new ItemStack(BALSA_PLANKS)).appendItems(stacks -> {
+            stacks.add(new ItemStack(MACHETE));
+            stacks.add(new ItemStack(BALSA_PLANKS));
+        }).build();
+    }
 }
