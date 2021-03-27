4749ca27026b7bd2ff0ea3becf93f960b1ed8685
kraken <lennyj2468@gmail.com>
2021-03-27 15:07:49 +0000

    failed to add tiki torch, someone correct me please

--- src/main/java/tropics/registry/TropicsBlocks.java
+++ src/main/java/tropics/registry/TropicsBlocks.java
@@ -2,12 +2,12 @@ package tropics.registry;
 
 import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
-import net.minecraft.block.Block;
-import net.minecraft.block.Material;
+import net.minecraft.block.*;
 import net.minecraft.item.BlockItem;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
 import net.minecraft.util.registry.Registry;
+
 
 import static tropics.Tropics.MODID;
 
@@ -17,13 +17,15 @@ public class TropicsBlocks
     public static final Block BALSA_PLANKS = new Block(
             FabricBlockSettings.of(Material.WOOD).strength(1.0F, 1.5F).sounds(BlockSoundGroup.WOOD));
 
-    public static final Identifier BALSA_PLANKS_ID = new Identifier("tropics", "balsa_planks");
+    public static final Block TIKI_TORCH = new Block(AbstractBlock.Settings.copy(Blocks.TORCH));
 
     public static void registerBlocks()
     {
-        Registry.register(Registry.ITEM, BALSA_PLANKS_ID, new BlockItem(BALSA_PLANKS, new FabricItemSettings()));
         Registry.register(Registry.BLOCK, new Identifier(MODID, "basla_planks"), BALSA_PLANKS);
         Registry.register(Registry.ITEM, new Identifier(MODID, "basla_planks"), new BlockItem(BALSA_PLANKS, new FabricItemSettings()));
+
+        Registry.register(Registry.BLOCK, new Identifier(MODID, "tiki_torch"), TIKI_TORCH);
+        Registry.register(Registry.ITEM, new Identifier(MODID, "tiki_torch"), new BlockItem(TIKI_TORCH, new FabricItemSettings()));
     }
 
 }
